c728c813a01c10615d838b5e6f6caa08a85c6bf6
Hala Zaki <zakihala9@gmail.com>
2025-06-05 17:56:59 +0500

    JS Assignment 5

---
+++ JS Assignments/Chap 21-25/app.js
@@ -0,0 +1,158 @@
+// var firstName = prompt("Write your first name");
+// var lastName = prompt("Write your last name");
+// document.writeln(`Your full name is ${firstName} ${lastName}.`);
+
+
+
+
+// var mobileModel = prompt("Write your favourite mobile model");
+// document.writeln(`My favourite phone is: ${mobileModel}`+"<br>");
+// document.writeln("Length of string: " + mobileModel.length);
+
+
+
+
+// var country = "Pakistani"
+// document.writeln(`String ${country}`+ "<br>");
+// document.writeln("Index of 'n': " + country.indexOf("n"));
+
+
+
+
+// var letter = "Hello World";
+// document.writeln(`String: ${letter}`+ "<br>");
+// document.writeln("Last index of 'l' : " + letter.lastIndexOf("l"));
+
+
+
+
+// var letter = "Pakistani";
+// document.writeln(`String: ${letter}`+ "<br>");
+// document.writeln("Character at index 3: " + letter.charAt(3));
+
+
+
+
+
+// var firstName = prompt("Write your first name");
+// var lastName = prompt("Write your last name");
+// var fulName = firstName.concat(" " + lastName);
+// document.writeln(`Your full name is ${fulName}`);
+
+
+
+
+// var city = "Hyderabad";
+// var newCity = city.replace("Hyder", "Islam");
+// document.writeln(`City: ${city}`+ "<br> After replacement: " + `${newCity}`);
+
+
+
+
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var newMessage = message.replaceAll("and", "&");
+// document.writeln(newMessage);
+
+
+
+
+// var a = "472";
+// document.writeln(`Value: ${a}` + "<br> Type: " + typeof a + "<br>");
+// var b = +a;
+// document.writeln(`Value: ${b}` + "<br> Type: " + typeof b );
+
+
+
+
+
+// var userInput = prompt("Write the name of your favourite food");
+// document.writeln(`User input: ${userInput}`+ "<br>");
+// document.writeln("Uppercase: " + userInput.toUpperCase());
+
+
+
+// var userInput = prompt("Write the name of your favourite food");
+// document.writeln(`User input: ${userInput}`+ "<br>");
+// document.writeln("Uppercase: " + userInput.t());
+
+
+
+
+
+// var userInput = prompt("Write your favourite food name");
+// var food = userInput.toLowerCase();
+// var newFood = food.replace(food.charAt(0), food.charAt(0).toUpperCase());
+// document.writeln(`User input: ${userInput}`+ "<br> Title case: " + newFood);
+
+
+
+
+
+
+// var numb = 35.36;
+// var str = numb.toString();
+// var newStr = str.replace(".", "");
+// document.writeln(`Number: ${numb}` + "<br>");
+// document.writeln(`Result: ${newStr}`);
+
+
+
+
+
+// var nameCheck = false;
+// var userName = prompt("Write your username");
+// for(var i = 0; i < userName.length; i++) {
+//    var check = userName.charCodeAt(i);
+//    if(check == 33 || check == 44 || check == 46 ||  check == 64) {
+//       userName = prompt("Please enter a valid user name");
+//       nameCheck = true;
+//    } 
+// }
+// if (nameCheck == false) {
+//    document.writeln(userName);
+// }
+
+
+
+
+
+
+// var productFound = false;
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/maa'm");
+// for (var i =0; i < arr.length; i++) {
+//     if (userInput.toLowerCase() == arr[i].toLowerCase()) {
+//         document.writeln(`${userInput} is available at index ${i} in our bakery.`);
+//         productFound = true;
+//     }
+// }
+// if ( productFound ==false) {
+//     document.writeln(`We are sorry. ${userInput} is not available in our bakery.`);
+// }
+
+
+
+
+
+// var university = "University of Karachi";
+// var str = university.split("");
+// console.log(str);
+
+
+
+
+// var input = prompt("Write your city name");
+// document.writeln("User input: " + input + "<br>");
+// var input2 = input.charAt(input.length-1);
+// document.writeln("Last character of input: " + input2);
+
+
+
+
+
+
+
+
+
+
