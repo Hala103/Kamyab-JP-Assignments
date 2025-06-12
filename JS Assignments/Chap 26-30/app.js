d875e8634cb17ae2b730e70a93a518f2f9f54905
Hala Zaki <zakihala9@gmail.com>
2025-06-12 22:35:25 +0500

    JS Assignment 6

---
+++ JS Assignments/Chap 26-30/app.js
@@ -0,0 +1,74 @@
+// var num = +prompt("Write a number");
+// var num1 = Math.round(num);
+// var num2 = Math.floor(num);
+// var num3 = Math.ceil(num);
+
+// document.writeln("number: " + num + "<br>");
+// document.writeln("roundoff value: " + num1 + "<br>");
+// document.writeln("floor value: " + num2 + "<br>");
+// document.writeln("ceil value: " + num3 + "<br>");
+
+
+
+
+
+
+// var num = +prompt("Write a  negative number in decimel");
+// var num1 = Math.round(num);
+// var num2 = Math.floor(num);
+// var num3 = Math.ceil(num);
+
+// document.writeln("number: " + num + "<br>");
+// document.writeln("roundoff value: " + num1 + "<br>");
+// document.writeln("floor value: " + num2 + "<br>");
+// document.writeln("ceil value: " + num3 + "<br>");
+
+
+
+// var a = prompt("Enter a number");
+// var b = Math.abs(a);
+// document.writeln("The absolute value of " + a + " is " + b);
+
+
+
+// var dice = Math.floor(Math.random() * 6) + 1
+// document.writeln("random dice value: " + dice );
+
+
+
+// var value = Math.floor(Math.random() * 2) + 1;
+// if (value == 1) {
+//     document.writeln(value + "<br> random coin value: Tails" );
+// } else {
+//     document.writeln(value + "<br> random coin value: Heads" );
+// }
+
+
+
+
+
+
+// var num = Math.floor(Math.random()*100) + 1;
+// document.writeln("random value between 1 and 100: " + num);
+
+
+
+
+
+var weight = prompt("Enter your weight");
+document.writeln(parseInt("The weight of user is " + weight + " kilograms"));
+
+
+
+
+// var secretNumber = 6;
+// var guessNumber = prompt("Guess the number between 1 - 10");
+// var guessNumber1 = Number(guessNumber);
+// if (guessNumber1 == secretNumber) {
+//     alert("Congratulation, your number is correct");
+// } else {
+//     alert("Wrong! Please try again");
+// }
+
+
+
