4f11fccb297a1c238ff9de219899689a6d8f24b7
Hala Zaki <zakihala9@gmail.com>
2025-06-26 21:31:13 +0500

    JS Assignment 10

---
+++ JS Assignments/Chap 43-48/app.js
@@ -0,0 +1,16 @@
+function mobileclick() {
+    alert("Thanks for purchasing from us");
+}
+
+
+
+
+   var  count = 0;
+
+function counting(num) {
+    count = count + num;
+   document.getElementById("counter").textContent = count;
+}
+
+
+
